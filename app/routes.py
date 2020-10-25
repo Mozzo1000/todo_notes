1dd0753b6dd2de94e10b531fe78e305c9d84c407
Andreas Backström <mozzo242@gmail.com>
2020-10-25 17:51:29 +0100

    Add login, logout and registration routes

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,46 @@
 from app import app, db
+from app.models import User
+from app.forms import LoginForm, RegisterForm
+from flask import render_template, redirect, url_for, flash
+from flask_login import current_user, login_user, logout_user
+
+
 @app.route('/')
 def index():
     return render_template('index.html', title='Home')
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid email or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign in', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, name=form.name.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Register complete, please login')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
