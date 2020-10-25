78b3fd1ec464a2f816c5d8fa7c84e0819e37d863
Andreas Backström <mozzo242@gmail.com>
2020-10-25 17:47:36 +0100

    Add secret key to config

--- app/config.py
+++ app/config.py
@@ -7,3 +7,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('TODO_NOTE_DATABASE_URL') or \
                              'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('TODO_NOTE_SECRET_KEY') or 'random_secret_key_here_please'
