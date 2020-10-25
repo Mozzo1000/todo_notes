605e2aab3ad4738ec627fbb2160cc9da17d0b771
Andreas Backström <mozzo242@gmail.com>
2020-10-25 15:55:13 +0100

    Add relations between notes and user
    Also set created_at and due_date to be indexed.

--- app/models.py
+++ app/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
     email = db.Column(db.String(240), index=True, unique=True)
     name = db.Column(db.String(240), index=True)
     password = db.Column(db.String(512))
+    notes = db.relationship('Notes', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -13,9 +14,9 @@ class User(db.Model):
     
 class Notes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    owner_id = db.Column(db.Integer)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     title = db.Column(db.String(120))
     content = db.Column(db.String(1024))
-    created_at = db.Column(db.DateTime)
-    due_date = db.Column(db.DateTime)
+    created_at = db.Column(db.DateTime, index=True)
+    due_date = db.Column(db.DateTime, index=True)
     tags = db.Column(db.String)
